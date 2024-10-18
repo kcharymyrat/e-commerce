3f7e0c558d079db572d297fc20cbab2e43b4f993
Charymyrat Garryyev <kcharymyrat@gmail.com>
2024-10-19 00:00:30 +0500

    user admin handlers are done

--- internal/handlers/users_admin_handlers.go
+++ internal/handlers/users_admin_handlers.go
@@ -9,6 +9,7 @@ import (
 	"github.com/go-playground/validator/v10"
 	"github.com/jackc/pgx/v5/pgconn"
 	"github.com/kcharymyrat/e-commerce/api/requests"
+	"github.com/kcharymyrat/e-commerce/api/responses"
 	"github.com/kcharymyrat/e-commerce/internal/app"
 	"github.com/kcharymyrat/e-commerce/internal/common"
 	"github.com/kcharymyrat/e-commerce/internal/constants"
@@ -90,6 +91,206 @@ func GetUsersAdminHandler(app *app.Application) http.HandlerFunc {
 
 		res := mappers.UserToUserAdminResponse(user)
 		err = common.WriteJson(w, http.StatusOK, types.Envelope{"user": res}, nil)
+		if err != nil {
+			common.ServerErrorResponse(app.Logger, localizer, w, r, err)
+		}
+	}
+}
+
+func ListUsersAdminHandler(app *app.Application) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		valTrans := r.Context().Value(constants.ValTransKey).(ut.Translator)
+		localizer := r.Context().Value(constants.LocalizerKey).(*i18n.Localizer)
+
+		filters := requests.UsersAdminFilters{}
+		err := common.ReadJSON(w, r, filters)
+		if err != nil {
+			common.BadRequestResponse(app.Logger, localizer, w, r, err)
+			return
+		}
+
+		err = app.Validator.Struct(filters)
+		if err != nil {
+			errs := err.(validator.ValidationErrors)
+			transErrs := make(map[string]string)
+			for _, e := range errs {
+				transErrs[e.Field()] = e.Translate(valTrans)
+			}
+			common.FailedValidationResponse(app.Logger, w, r, transErrs)
+			return
+		}
+
+		users, metadata, err := services.ListUsersService(app, &filters)
+		if err != nil {
+			switch {
+			case errors.Is(err, common.ErrRecordNotFound):
+				common.NotFoundResponse(app.Logger, localizer, w, r)
+			default:
+				common.ServerErrorResponse(app.Logger, localizer, w, r, err)
+			}
+			return
+		}
+
+		resSlice := []*responses.UserAdminResponse{}
+		for _, user := range users {
+			res := mappers.UserToUserAdminResponse(user)
+			resSlice = append(resSlice, res)
+		}
+
+		err = common.WriteJson(w, http.StatusOK, types.Envelope{
+			"metadata": metadata,
+			"result":   resSlice,
+		}, nil)
+		if err != nil {
+			common.ServerErrorResponse(app.Logger, localizer, w, r, err)
+		}
+
+	}
+}
+
+func UpdateUserAdminHandler(app *app.Application) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		valTrans := r.Context().Value(constants.ValTransKey).(ut.Translator)
+		localizer := r.Context().Value(constants.LocalizerKey).(*i18n.Localizer)
+
+		id, err := common.ReadUUIDParam(r)
+		if err != nil {
+			common.ServerErrorResponse(app.Logger, localizer, w, r, err)
+			return
+		}
+
+		input := requests.UserAdminUpdate{}
+		err = common.ReadJSON(w, r, input)
+		if err != nil {
+			common.BadRequestResponse(app.Logger, localizer, w, r, err)
+			return
+		}
+
+		err = app.Validator.Struct(input)
+		if err != nil {
+			errs := err.(validator.ValidationErrors)
+			translatedErrs := make(map[string]string)
+			for _, e := range errs {
+				translatedErrs[e.Field()] = e.Translate(valTrans)
+			}
+			common.FailedValidationResponse(app.Logger, w, r, translatedErrs)
+			return
+		}
+
+		user, err := services.GetUserService(app, id)
+		if err != nil {
+			switch {
+			case errors.Is(err, common.ErrRecordNotFound):
+				common.NotFoundResponse(app.Logger, localizer, w, r)
+			default:
+				common.ServerErrorResponse(app.Logger, localizer, w, r, err)
+			}
+			return
+		}
+
+		err = services.UpdateUsersService(app, &input, user)
+		if err != nil {
+			if pgErr, ok := err.(*pgconn.PgError); ok {
+				e := common.TransformPgErrToCustomError(pgErr)
+				HandlePGErrors(app.Logger, localizer, w, r, e)
+				return
+			}
+			common.ServerErrorResponse(app.Logger, localizer, w, r, err)
+			return
+		}
+
+		res := mappers.UserToUserAdminResponse(user)
+		err = common.WriteJson(w, http.StatusOK, types.Envelope{"user": res}, nil)
+		if err != nil {
+			common.ServerErrorResponse(app.Logger, localizer, w, r, err)
+		}
+
+	}
+}
+
+func PartialUpdateUserAdminHandler(app *app.Application) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		valTrans := r.Context().Value(constants.ValTransKey).(ut.Translator)
+		localizer := r.Context().Value(constants.LocalizerKey).(*i18n.Localizer)
+
+		id, err := common.ReadUUIDParam(r)
+		if err != nil {
+			common.ServerErrorResponse(app.Logger, localizer, w, r, err)
+			return
+		}
+
+		input := requests.UserAdminPartialUpdate{}
+		err = common.ReadJSON(w, r, input)
+		if err != nil {
+			common.BadRequestResponse(app.Logger, localizer, w, r, err)
+			return
+		}
+
+		err = app.Validator.Struct(input)
+		if err != nil {
+			errs := err.(validator.ValidationErrors)
+			translatedErrs := make(map[string]string)
+			for _, e := range errs {
+				translatedErrs[e.Field()] = e.Translate(valTrans)
+			}
+			common.FailedValidationResponse(app.Logger, w, r, translatedErrs)
+			return
+		}
+
+		user, err := services.GetUserService(app, id)
+		if err != nil {
+			switch {
+			case errors.Is(err, common.ErrRecordNotFound):
+				common.NotFoundResponse(app.Logger, localizer, w, r)
+			default:
+				common.ServerErrorResponse(app.Logger, localizer, w, r, err)
+			}
+			return
+		}
+
+		err = services.PartialUpdateUsersService(app, &input, user)
+		if err != nil {
+			if pgErr, ok := err.(*pgconn.PgError); ok {
+				e := common.TransformPgErrToCustomError(pgErr)
+				HandlePGErrors(app.Logger, localizer, w, r, e)
+				return
+			}
+			common.ServerErrorResponse(app.Logger, localizer, w, r, err)
+			return
+		}
+
+		res := mappers.UserToUserAdminResponse(user)
+		err = common.WriteJson(w, http.StatusOK, types.Envelope{"user": res}, nil)
+		if err != nil {
+			common.ServerErrorResponse(app.Logger, localizer, w, r, err)
+		}
+
+	}
+}
+
+func DeleteUserAdminHandler(app *app.Application) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		// valTrans := r.Context().Value(constants.ValTransKey).(ut.Translator)
+		localizer := r.Context().Value(constants.LocalizerKey).(*i18n.Localizer)
+
+		id, err := common.ReadUUIDParam(r)
+		if err != nil {
+			common.ServerErrorResponse(app.Logger, localizer, w, r, err)
+			return
+		}
+
+		err = services.DeleteUserService(app, id)
+		if err != nil {
+			switch {
+			case errors.Is(err, common.ErrRecordNotFound):
+				common.NotFoundResponse(app.Logger, localizer, w, r)
+			default:
+				common.ServerErrorResponse(app.Logger, localizer, w, r, err)
+			}
+			return
+		}
+
+		err = common.WriteJson(w, http.StatusOK, types.Envelope{"message": "user successfully deleted"}, nil)
 		if err != nil {
 			common.ServerErrorResponse(app.Logger, localizer, w, r, err)
 		}
